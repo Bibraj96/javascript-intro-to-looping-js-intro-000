b49b308d51d60db99504d150afa104e6e47cb634
Blendi Ibraj <blendi.ibraj96@gmail.com>
2020-01-02 21:36:56 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,16 +8,3 @@ function forLoop(array) {
   }
   return array;
 }
-
-function whileLoop(n) {
-  var i = 0;
-
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
-
-do {
-  console.log("I run once regardless");
-} while (incrementVariable() < num);
-}
